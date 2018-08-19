94da173a036365ba86d0c8b578922faa096070e3
maguoying <vskyma@163.com>
2018-08-19 19:14:03 +0800

    引入JUnit4

--- src/com/maguoying/base/DateUtil.java
+++ src/com/maguoying/base/DateUtil.java
@@ -4,11 +4,18 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.junit.Test;
+
 public class DateUtil {
 	public static Date getDate() {
 		Calendar calendar = Calendar.getInstance();
 		Date date = calendar.getTime();
 		return date;
+	}
+	
+	@Test
+	public void test(){
+		System.out.println(getDate());
 	}
 	
 	public static String getFormatDate(String formatString) {
